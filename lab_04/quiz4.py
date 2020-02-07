416eef4e082b804af63048b36af57415beb80e99
foqiao <foqiao89@hotmail.com>
2020-02-06 19:09:10 -0800

    Chris, I trying quiz4 at home now. I don't know why it returns nothing.

--- lab_04/quiz4.py
+++ lab_04/quiz4.py
@@ -1,13 +1,12 @@
 def statistics(stats_test):
-    stats_test = [1, 23, 56, 2, 5, 6]
-    stats_test_result = []
-    stats_test_result[0] = stats_test.count()
-    stats_test_result[1] = min(stats_test)
-    stats_test_result[2] = max(stats_test)
-    stats_test_result[3] = int(sum(stats_test)) / int(stats_test.count())
-    stats_test_average = max(stats_test) - min(stats_test)
-    stats_test_result[4] = range(min(stats_test), max(stats_test), stats_test.index(0, -1) + 1)
-    return stats_test_result
+    stats_test = []
+    for stats_test in range(len(stats_test) - 1):
+        stats_test[0] = stats_test.count()
+        stats_test[1] = min(stats_test)
+        stats_test[2] = max(stats_test)
+        stats_test[3] = int(sum(stats_test)) / int(stats_test.count())
+        stats_test[4] = range(min(stats_test), max(stats_test), stats_test.index(0, -1))
+    return stats_test
 
-if __name__ == '__main__':
-    statistics(stats_test=True)
+stats_test_input = [1, 23, 56, 2, 5]
+statistics(stats_test_input)
