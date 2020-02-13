1b083e813941444f9a509a72a09442b61176ef20
foqiao <foqiao89@hotmail.com>
2020-02-13 13:42:51 -0800

    Chris, this is the update for Dungen and Dragon assignment two from COMP 1510. Tell me if there is anything wrong on this program. Thanks

--- a2/D&D.py
+++ a2/D&D.py
@@ -1,12 +1,7 @@
 import random
 import string
-from numpy import NaN
-
 
 def roll_dice(number_of_roll, number_of_side):
-    number_of_roll = random.randint(1, 3)
-    number_of_side = random.randint(1, 12)
-
     sum_roll_dice = number_of_roll * number_of_side
     return sum_roll_dice
 
@@ -18,11 +13,11 @@ def generate_name(syllable):
     syllable = random.choice(vowel) + random.choice(consonent)
     return syllable
 
-def create_character(character_syllable):
-    if character_syllable < 0:
+def create_character(character_syllable, second_character_syllables):
+    if character_syllable or second_character_syllables < 0:
         print("Warning")
         return None
-    elif 1 <= character_syllable <= 10:
+    elif 1 <= character_syllable or second_character_syllables <= 10:
         character_dictionary = {
             1: 'Elf',
             2: 'Orc',
@@ -30,16 +25,20 @@ def create_character(character_syllable):
             4: 'Barbarian',
             5: 'Wizard',
             6: 'Alagon',
-            7: 'Sorrow'
+            7: 'Sorrow',
             8: 'Mayday',
-            9: 'Katniss Everdeen'
+            9: 'Katniss Everdeen',
             10: 'Wu'
         }
+        character_syllable = random.choice(character_dictionary)
+        second_character_syllables = random.choice(second_character_syllables)
+        return character_syllable
+        return second_character_syllables
     else:
         print("not yet")
         return None
 
-def choose_inventory(outfit):
+def choose_inventory():
 
     outfit = {
         "sword": 1,
@@ -51,83 +50,68 @@ def choose_inventory(outfit):
         "Upgrade defense": 7,
         "more": 8,
         "Special Curse": 9,
-        "-1": exit(outfit)
+        "-1": exit(choose_inventory())
     }
 
     return outfit
 
-    purchased_items = {
+    outfits = int(input("Please enter the index number of items you want to buy: "))
+
+    purchased_item = {
         "knife": 1,
         "shield": 2
     }
 
-    if shop_items.values() >= 10 || shop_items.values() <= 0:
-        purchased_items = random.randrange(outfit.values(3), outfit.values(9));
+    if int(outfits) >= 10 & int(outfits) <= 0:
+        purchased_item = random.randrange(outfit.values(3), outfit.values(9))
 
-
-def combat_round(Player_spec, number_of_sides, number_of_rolls):
+def combat_round():
+    picked_character = character_syllables
+    picked_second_character = second_character_syllable
     picked_character = {
         "health": range(100, 0, -1),
-        "weapon": bag_list
+        "weapon": purchased_items
     }
     picked_second_character = {
         "health": range(100, 0, -1),
-        "weapon": bag_list
+        "weapon": purchased_items
     }
+
     return picked_character
-    Monster_specs = {
+    return picked_second_character
+
+    monster = {
         "health": range(100, 0, -1),
         "weapon": None
     }
-    for number_of_sides in range(1, 12):
-        if number_of_rolls
+
+    return monster
+
+    for number_of_side in range(1, 12):
+        player_roll = random.choice(number_of_sides)
+        monster_roll = random.choice(number_of_sides)
+        if player_roll == monster_roll:
+            player_roll = random.choice(number_of_sides)
+            monster_roll = random.choice(number_of_sides)
+        elif player_roll < monster_roll:
+            player_roll["health"] -= 1
+        elif player_roll > monster_roll:
+            monster_roll["health"] -= 1
+        else:
+            return None
+
+
+def purchased_items():
+    pass
 
 if __name__ == '__main__':
-    player_response = int(input("Please enter the side of your first roll(e.g. 1, 2): "))
-    player_second_response = int(input("Please enter the side of your second roll(e.g. 1, 2): "))
-    player_third_response = int(input("Please enter the side of your third roll(e.g. 1, 2): "))
+    number_of_rolls = random.randint(1, 3)
+    number_of_sides = random.randint(1, 12)
+    roll_dice(number_of_rolls, number_of_sides)
 
-    final_syllable_output_index = str(player_response) + str(player_second_response) + str(player_third_response)
+    character_syllables = int(input("Please enter a number between 1 and 10 randomly: "))
+    second_character_syllable = int(input("Please enter a number between 1 and 10 randomly: "))
+    create_character(character_syllables, second_character_syllable)
 
-    number_of_roll = range(1, 3, 1)
-    number_of_side = range(1, 12, 1)
 
-    roll_dice(number_of_roll, number_of_side, final_syllable_output_index)
-
-    syllable = final_syllable_output_index
-
-    generate_name(syllable)
-
-    create_character(syllable)
-
-    character1 = final_syllable_output_index
-
-    print_character(character1, player_response, player_second_response, player_third_response)
-
-    outfit1 = str(input("Please enter the outfit you want to have by initial number(e.g. 1 for 1.Sword): "))
-
-    choose_inventory(outfit1)
-
-    Player_roll = int(input("Please enter the number of rolls you want to have:"))
-    Monster_roll = random.choice(number_of_roll)
-
-    bag_list = int(input("Please enter the index value of the weapon you want from 0-first_weapon to ##-last " +
-                              "weapon: "))
-
-    Player_specs1 = {
-        "health": range(100, 0, -1),
-        "weapon": bag_list
-    }
-    del Player_specs1["weapon"]
-
-    current_bag_list1 = {
-        "knife": 1,
-        "shield": 2
-    }
-
-    if int(bag_list) == range(current_bag_list1.index(0), current_bag_list1.index(-1)):
-        Player_specs1["weapon"] = current_bag_list1.index(int(bag_list))
-    else:
-        print("Invalid index value")
-
-    combat_round(Player_specs1, Player_roll, Monster_roll)
+    choose_inventory()
