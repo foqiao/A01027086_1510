18de5633cdb401b6b97a6563ec8c857e3adda6d1
foqiao <foqiao89@hotmail.com>
2020-02-19 15:15:25 -0800

    Chris, an update version of dijkstra.py. The script can't return anything. I have no idea why it is like that.

--- lab_03/dijkstra.py
+++ lab_03/dijkstra.py
@@ -3,14 +3,13 @@ import random
 
 def dijkstra():
     dutch = ["white", "white", "white", "red", "red", "blue", "blue"]
-    flag = random.choice(dutch)
-    color_arrangement = []
-    if flag == dutch[5:6]:
-        color_arrangement[0:1] = flag
-    elif flag == dutch[0:2]:
-        color_arrangement[2:4] = flag
-    elif flag == dutch[3:4]:
-        color_arrangement[5:6] = flag
+    color_arrangement = ""
+    if dutch[5:6] == random.choice(dutch):
+        color_arrangement.append(random.choice(dutch))
+    elif dutch[0:2] == random.choice(dutch):
+        color_arrangement.append(random.choice(dutch))
+    elif dutch[3:4] == random.choice(dutch):
+        color_arrangement.append(random.choice(dutch))
 
     return color_arrangement
 
