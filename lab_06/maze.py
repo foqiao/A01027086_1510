57d934b00dd7ac56c0bf066e90fe10d6657b811c
foqiao <foqiao89@hotmail.com>
2020-03-06 13:01:12 -0800

    Chris, the character function has a variable move_length. It is on line 22. I declared it in another function as a global variable. Why it's still undefined by my PyCharm?

--- lab_06/maze.py
+++ lab_06/maze.py
@@ -1,32 +1,27 @@
-def game():
-    board()
-    while not check_if_exit_reached():
-        move_length = int(input("Please enter how many steps you want to move horizontally: "))
-        move_width = int(input("Please enter how many steps you want to move vertically: "))
-        character()
-        valid_move(board, character)
-        if valid_move:
-            check_if_exit_reached()
-        else:
-            game()
-
 def board():
+    global length, width, min_length, min_width
     length = 5
     min_length = 0
     width = 5
     min_width = 0
-    print("The board is %d * %d." % (length, length))
+    print("The board is %d * %d." % (length, width))
     print("The minimum coordinate is (%d,%d)." % (min_length, min_width))
     print("The maximum coordinate is (%d,%d)." % (length, width))
+    print("You current position is (%d,%d)." % (min_length, min_width))
+
+def user_choice():
+    global move_length, move_width
+    move_length = int(input("Please enter how many steps you want to move horizontally: "))
+    move_width = int(input("Please enter how many steps you want to move vertically: "))
+    total_Length = move_length + total_length
+    total_Width = move_width + total_width
+    print("(%d,%d)" % (total_Length, total_Width))
 
 def character():
-    moving_log = {}
-    steps = range(0, 25)
-    for i in steps:
-        total_length = move_length + moving_log[i][1]
-        total_width = move_width + moving_log[i][3]
-        moving_log.update({i: "(%d,%d)" % (total_length, total_width)})
-        print(moving_log[i])
+    global total_length, total_width
+    total_length = move_length + min_length
+    total_width = move_width + min_width
+    print("(%d,%d)" % (total_length, total_width))
 
 def valid_move(userBoard, userCharacter):
     character()
@@ -35,13 +30,22 @@ def valid_move(userBoard, userCharacter):
             pass
 
 def check_if_exit_reached():
-    character()
     current_position = total_length * total_width
     final_exit = length * width
     if current_position >= final_exit:
         print("You Win")
-    else:
-        print("More steps needed to reach the exit.")
+
+def game():
+    board()
+    character()
+    while not check_if_exit_reached():
+        valid_move(board, character)
+        if valid_move:
+            check_if_exit_reached()
+            if check_if_exit_reached:
+                print("You win")
+            else:
+                print("You haven't win yet.")
 
 if __name__ == '__main__':
     game()
