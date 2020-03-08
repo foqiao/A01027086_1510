223ef04ac0634e4577a5535790f2b079b2f6b67c
foqiao <foqiao89@hotmail.com>
2020-03-08 11:53:44 -0700

    Chris, this is all the corrected assignments from the previous class.

--- lab_03/dijkstra.py
+++ lab_03/dijkstra.py
@@ -2,13 +2,13 @@ def dijkstra():
     dutch = ["white", "white", "white", "red", "red", "blue", "blue"]
     color_arrangement = []
     for color_order in range(0, 7, 1):
-        if dutch[color_order] == "blue":
-            color_arrangement.append("blue" * 2)
-        elif dutch[color_order] == "white":
-            color_arrangement.append("white" * 3)
+        if dutch[color_order] == "white":
+            color_arrangement.append("white")
         elif dutch[color_order] == "red":
-            color_arrangement.append("red" * 2)
-    return color_arrangement[0] + color_arrangement[1] + color_arrangement[2]
+            color_arrangement.append("red")
+        elif dutch[color_order] == "blue":
+            color_arrangement.append("blue")
+    return color_arrangement
 
 if __name__ == '__main__':
     dijkstra()
