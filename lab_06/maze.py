e96ce60628f8afd7936f0f6aa2cdf822ecadbd31
foqiao <foqiao89@hotmail.com>
2020-03-08 17:36:22 -0700

    Chris, the length and width of every steps will return the value at power of 5. For example, (3,2) is (3 ** 5, 2 ** 5) rather than just (3,2). Could you tell me why?

--- lab_06/maze.py
+++ lab_06/maze.py
@@ -1,5 +1,7 @@
+from numpy import NaN
+
 def board():
-    global length, width, min_length, min_width
+    global length, width
     length = 5
     min_length = 0
     width = 5
@@ -9,90 +11,55 @@ def board():
     print("The maximum coordinate is (%d,%d)." % (length, width))
     print("You current position is (%d,%d)." % (min_length, min_width))
 
-def character_first_move():
-    global total_length, total_width
-    total_length = move_length + min_length
-    total_width = move_width + min_width
-
-def user_first_choice():
+def user_choice():
     global move_length, move_width
     move_length = int(input("Please enter how many steps you want to move horizontally: "))
     move_width = int(input("Please enter how many steps you want to move vertically: "))
-    character_first_move()
-    total_Length = total_length
-    total_Width = total_width
-    print("(%d,%d)" % (total_Length, total_Width))
 
-def user_second_choice():
-    global move_length1, move_width1
-    move_length1 = int(input("Please enter how many steps you want to move horizontally: "))
-    move_width1 = int(input("Please enter how many steps you want to move vertically: "))
-    total_Length = move_length1 + total_length
-    total_Width = move_width1 + total_width
-    print("(%d,%d)" % (total_Length, total_Width))
+def character():
+    global move_length, move_width
+    length_log = []
+    width_log = []
+    length_log.append(move_length)
+    width_log.append(move_width)
+    for i in (0, len(length_log) - 1):
+        for j in (0, len(width_log) - 1):
+            move_length *= length_log[i]
+            move_width *= width_log[j]
+    print("(%d,%d)" % (move_length, move_width))
 
-def user_choice():
-    global move_length1, move_width1
-    move_length1 = int(input("Please enter how many steps you want to move horizontally: "))
-    move_width1 = int(input("Please enter how many steps you want to move vertically: "))
-    total_Length = move_length1 + total_length
-    total_Width = move_width1 + total_width
-    print("(%d,%d)" % (total_Length, total_Width))
-
-def character_second_move():
-    global total_LENGTH, total_WIDTH
-    total_LENGTH = move_length1 + total_length
-    total_WIDTH = move_width1 + total_width
-
-def first_valid_move(userBoard, userCharacter):
-    user_first_choice()
-    if length > total_length > 0:
-        if width > total_width > 0:
-            pass
-
-def valid_move(userBoard, userCharacter):
-    user_choice()
-    if length > total_length > 0:
-        if width > total_width > 0:
-            pass
-        else:
-            game()
+def valid_move(board1, user_choice1):
+    if user_choice1 > board1:
+        print("invalid")
+        user_choice()
+    elif user_choice1 < board1:
+        pass
+    elif user_choice1 == NaN:
+        print("try again")
+        user_choice()
 
 def check_if_exit_reached():
-    current_position = total_length * total_width
+    current_position = move_length * move_width
     final_exit = length * width
     if current_position >= final_exit:
         print("You Win")
 
-def game():
-    user_first_choice()
-    character_first_move()
-    while not check_if_exit_reached():
-        first_valid_move(board, character_first_move)
-        if first_valid_move:
-            check_if_exit_reached()
-            game1()
-        else:
-            print("invalid")
-            game()
-
 def game1():
     user_choice()
-    while not check_if_exit_reached():
-        valid_move(board, character_first_move)
+    character()
+    check_if_exit_reached()
+    while not check_if_exit_reached:
+        valid_move(board, user_choice)
         if valid_move:
             check_if_exit_reached()
+            game1()
         else:
-            print("invalid")
             game1()
 
-def main():
+def game():
     board()
-    game()
-
-def main1():
     game1()
 
 if __name__ == '__main__':
-    main()
-    main1()
+    game()
+    game1()
