4ed779b1bc2d3d9d9f55917d23747c4299d9fa61
foqiao <foqiao89@hotmail.com>
2020-03-09 19:52:27 -0700

    The if-else statement doesn't print anything. I don't know why it is like that.

--- lab_02/corrected_lab_02.py
+++ lab_02/corrected_lab_02.py
@@ -1,5 +1,5 @@
 import random
-
+import string
 
 def roll_die(num_of_rolls, num_of_sides):
     rolled_sum = range(num_of_sides, num_of_rolls * num_of_sides - 1)
@@ -11,13 +11,9 @@ num_of_sides1 = int(input("Please enter how many sides you want your die to be: 
 roll_die(num_of_rolls1, num_of_sides1)
 
 def create_name(length):
-    name_string = []
-    letter = "abcdefghijklmnopqrstuvwxyz"
+    alpha = string.ascii_lowercase
+    letter_picker = random.choice(alpha)
 
-    letter_picker = random.choice(letter)
-    name_string.append(letter_picker)
 
-    if len(name_string) == length:
-        print(name_string)
 
-length1 = 
+length1 = int(input("Please enter the length of the name you want to have: "))
