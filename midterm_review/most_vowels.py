d3cff009babb1385be0b2e6a8a336347c4e2a38e
foqiao <foqiao89@hotmail.com>
2020-03-15 15:10:22 -0700

    the most vowels function still won't work

--- midterm_review/most_vowels.py
+++ midterm_review/most_vowels.py
@@ -1,9 +1,19 @@
-
-
 def most_vowels(tuple):
-    vowel = ['a', 'e', 'i', 'o', 'u']
-    tuple.count(vowel)
-
+    vowels_in_string = {}
+    tuple_range = range(0, len(tuple) + 1)
+    mul_vowels = range(1, len(tuple) + 1)
+    for i in tuple_range:
+        for j in mul_vowels:
+            if tuple[i] == 'a' * j:
+                vowels_in_string.update({i: tuple[i]})
+            elif tuple[i] == 'e' * j:
+                vowels_in_string.update({i: tuple[i]})
+            elif tuple[i] == 'i' * j:
+                vowels_in_string.update({i: tuple[i]})
+            elif tuple[i] == 'o' * j:
+                vowels_in_string.update({i: tuple[i]})
+            elif tuple[i] == 'u' * j:
+                vowels_in_string.update({i: tuple[i]})
 
 tuple_input = tuple(input("Please enter a combination of strings: "))
 most_vowels(tuple_input)
