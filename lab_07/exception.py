929fe93031d6f2bf7113b7fbfa47e111d05dcaa5
foqiao <foqiao89@hotmail.com>
2020-03-19 10:06:21 -0700

    Chris, this is my lab_07. There is a problem with the second function.

--- lab_07/exception.py
+++ lab_07/exception.py
@@ -6,7 +6,7 @@ def heron(num: int) -> float:
     :param num: any number inputs by user
     :return: square root(in float) of the input
     """
-    num_range = range(1, num)
+    num_range = range(-num, num)
     for number in num_range:
         quotient = (number + num / number) / 2
         result = (quotient + num / quotient) / 2
@@ -14,33 +14,35 @@ def heron(num: int) -> float:
             print(result)
             break
 
-try:
-    heron(1 / 0)
-except ZeroDivisionError:
-    print("No, I can't do it!")
-
 num_input = int(input("Please enter a number you want to find the sqrt of: "))
 heron(num_input)
 
-def findAnEvenNumber(num1: list):
+try:
+    num1 = 1 / 0
+    heron(num1)
+except ZeroDivisionError:
+    print("No, I can't do it!")
+    print(-1)
+
+def findAnEvenNumber(num2: list):
     """
     return first even value of the list
-    :param num1: a list of numbers ready to inspect by the function
+    :param num2: a list of numbers ready to inspect by the function
     :precondition: a list of random numbers, whatever user input, an odd/even mixed string
     :postcondition: return the first even number of the list
     :raise ValueError: the number that isn't even
     :return: the number has qualified by the function
     """
-    for i in range(0, len(num1)):
-        if num1[i] % 2 == 0:
-            print(i)
-            break
-
-try:
-    list_num = [1, 3]
-    findAnEvenNumber(list_num)
-except ValueError:
-    print("Sorry, it isn't an even number!")
+    for i in range(0, len(num2)):
+        if num2[i] % 2 == 0:
+            print(num2[i])
 
 num1_input = list(input("Please enter a list of number: "))
 findAnEvenNumber(num1_input)
+
+try:
+    list_num = [1]
+    for i1 in list_num:
+        findAnEvenNumber(list_num[i1])
+except ValueError:
+    print("Sorry, it isn't an even number!")
