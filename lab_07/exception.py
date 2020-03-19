07b6873f27bdaa2e6dfd7fee0d2a904290fe1225
foqiao <foqiao89@hotmail.com>
2020-03-19 14:43:46 -0700

    Chris, this is my lab_07. There is a problem with the second function.

--- lab_07/exception.py
+++ lab_07/exception.py
@@ -7,12 +7,33 @@ def heron(num: int) -> float:
     :raise ZeroDivisionError: 1 / 0
     :return: square root(in float) of the input
     """
-    num_range = range(1, num)
+    num_range = range(-num, num)
     for i in num_range:
         quotient = (i + num / i) / 2
         result = (quotient + num / quotient) / 2
-        if result ** 2 == num:
+        squared_result = result ** 2
+        if round(squared_result) == num:
             print(result)
+            break
 
-num_input = int(input("Please enter a number you want to find the sqrt of: "))
-heron(num_input)
+try:
+    num_input = 0
+except ZeroDivisionError:
+    print("No, I can't do that!")
+    print(-1)
+
+num_input1 = int(input("Please enter a number you want to find the sqrt of: "))
+heron(num_input1)
+
+def findAnEvenNumber(input_list: list):
+    for i in input_list:
+        if i % 2 == 0:
+            print(i)
+
+try:
+    input_list1 = [3]
+except ValueError:
+    print("No, I can't do that!")
+
+input_list2 = list(input("Please enter a random list of number: "))
+findAnEvenNumber(input_list2)
--- lab_07/test_heron.py
+++ lab_07/test_heron.py
@@ -5,4 +5,4 @@ from lab_07.exception import heron
 
 class Test(TestCase):
     def test_heron(self):
-        self.assertTrue(1.4142, heron(2))
+        self.assertTrue(1.41666666666, heron(2))
