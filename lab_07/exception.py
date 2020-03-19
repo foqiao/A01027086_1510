b5ae61dfc832eff2a8469eeb887031a138fe884e
foqiao <foqiao89@hotmail.com>
2020-03-19 14:04:21 -0700

    Chris, this is my lab_07. There is a problem with the second function.

--- lab_07/exception.py
+++ lab_07/exception.py
@@ -7,30 +7,12 @@ def heron(num: int) -> float:
     :raise ZeroDivisionError: 1 / 0
     :return: square root(in float) of the input
     """
-    num_range = range(-num, num)
-    for number in num_range:
-        quotient = (number + num / number) / 2
+    num_range = range(1, num)
+    for i in num_range:
+        quotient = (i + num / i) / 2
         result = (quotient + num / quotient) / 2
-        if round(result ** 2) == num:
+        if result ** 2 == num:
             print(result)
-            break
 
 num_input = int(input("Please enter a number you want to find the sqrt of: "))
 heron(num_input)
-
-def findAnEvenNumber(num2: list):
-    """
-    return first even value of the list
-    :param num2: a list of numbers ready to inspect by the function
-    :precondition: a list of random numbers, whatever user input, an odd/even mixed string
-    :postcondition: return the first even number of the list
-    :raise ValueError: the number that isn't even
-    :return: the number has qualified by the function
-    """
-    for i in num2:
-        if i % 2 == 0:
-            return i
-    raise ValueError
-
-num1_input = list(input("Please enter a list of number: "))
-findAnEvenNumber(num1_input)
