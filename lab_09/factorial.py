76d14b8ac19caa20eb3b97cbfdf1cea143bf34ca
foqiao <foqiao89@hotmail.com>
2020-03-31 12:23:30 -0700

    factorial.py update

--- lab_09/factorial.py
+++ lab_09/factorial.py
@@ -1,6 +1,9 @@
 import time
 
-
+"""
+timer function store the procedures needed for time consumes during factorial calculations happened on two different
+methods.
+"""
 def timer(func):
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()
@@ -17,6 +20,11 @@ def timer(func):
 
 @timer
 def factorial_iterative(value):
+    """
+    plain loop to calculate the factorial
+    :param value: input value from main function
+    :return: result returned after calculations finished
+    """
     global product
     for i in range(1, value + 1, 2):
         if i < value:
@@ -24,7 +32,12 @@ def factorial_iterative(value):
     result = product ** (value / 2)
     return result
 
-
+def factorial_recursive(value):
+    """
+    recursive way to calculate factorial
+    :param value: value from main function
+    :return: result return after calculation finished
+    """
 
 def main():
     value = int(input("Please enter a positive number from 1 to 100: "))
