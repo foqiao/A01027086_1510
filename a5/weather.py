880cf4aa9101296b329f04eaf6e094d662304e3e
foqiao <foqiao89@hotmail.com>
2020-04-06 18:46:49 -0700

    current update of A5(previous known as lab 11)

--- a5/weather.py
+++ a5/weather.py
@@ -1,44 +1,28 @@
+import requests
 import json
 
-import openweather
-from datetime import datetime
 
-import requests
+def get_forecast(url: str) -> None:
+    response = requests.get(url)
+    response.raise_for_status()
+    vancouver_weather = json.loads(response.text)
+    # print(vancouver_weather)
+    w = vancouver_weather['list']
+    print('Current weather in Vancouver:')
+    print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+    print()
+    print('Tomorrow:')
+    print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+    print()
+    print('Day after tomorrow:')
+    print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
 
-def api(day):
-   for i in range(1, int(day) + 1):
-        url = 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver&cnt=%d&appid=d8ecf789b325a' \
-              '7d40e4c0b03c57ff60d' % i
-        weather_API = requests.get(url)
-        json_converter = json.loads(weather_API.text)
-
-        # create client
-        ow = openweather.OpenWeather()
-
-        # historic weather
-        start_date = datetime(json_converter['cnt'])
-        end_date = datetime(json_converter['cnt'])
-        dt = datetime(json_converter.list[0]['dt'])
-        sunrise = datetime(json_converter.list[0]['sunrise'])
-        sunset = datetime(json_converter.list[0]['sunset'])
-        city_id = api.list.city.id
-        # default: hourly interval
-        print(ow.get_historic_weather(city_id, start_date, end_date))
-
-        # daily aggregates
-        print(ow.get_historic_weather(city_id, start_date, end_date, "day"))
-
-def weather_vancouver(i, json_converter):
-    w = json_converter['list']
-    print('Day: %d' % i)
-    print(f'humidity: {json_converter.list.humidity}')
-    print(f'sunrise: {sunrise}')
-    print(f'sunset: {sunset}')
-    print(f'daytime: {dt}')
 
 def main():
-    day = input("How many days do you want: ")
-    api(day)
+    vancouver = 6173331
+    api_key = "73e6dd36a2a073c414a629760ac198ce"
+    url = "http://api.openweathermap.org/data/2.5/forecast?id=6173331&APPID=73e6dd36a2a073c414a629760ac198ce"
+    get_forecast(url)
 
 if __name__ == '__main__':
     main()
