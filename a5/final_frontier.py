5621d5f8a5178a76ed46b4cffc6002c9c2d5baa1
foqiao <foqiao89@hotmail.com>
2020-04-10 17:24:33 -0700

    I finished final_frontier.py. Chris

--- a5/final_frontier.py
+++ a5/final_frontier.py
@@ -5,17 +5,17 @@ import json
 
 def final_frontier(url: str):
     response = requests.get(url)
-    with open(response.text, "r") as frontier:
-        for frontier_Read in frontier:
-            frontier_read = frontier.readline(1)
-            print(frontier)
-            time.sleep(300)
+    response.raise_for_status()
+    frontier = json.loads(response.text)
+    print(f"planet hostname: {frontier[0]['pl_hostname']}")
+    print(f"planet letter: {frontier[0]['pl_letter']}")
+    print(f"planet name: {frontier[0]['pl_name']}")
+    print(f"planet disc method: {frontier[0]['pl_discmethod']}")
+    time.sleep(300)
 
 def main():
-    url = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&format=ipac&where=pl_kepflag=1"
+    url = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json"
     final_frontier(url)
-    func = final_frontier(url)
-    recorder(func)
 
 if __name__ == '__main__':
     main()
