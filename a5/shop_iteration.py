a0ba9bd0ed043e249223355c89e66530dcfe69ca
foqiao <foqiao89@hotmail.com>
2020-04-17 11:27:39 -0700

    itertool for hackathon

---
+++ a5/shop_iteration.py
@@ -0,0 +1,21 @@
+import itertools
+
+def shop_iteration(shop_dict, item_dict):
+    it = itertools.cycle(shop_dict)
+    for key, value in it:
+        #key stores store's real-time specs
+        #value represents the current crowd size of the Costco
+        value = key['lineup']
+        maximum_limit = 100
+        print(f"{key}: {value}")
+        #if the line going beyond 100 meters
+        if value is maximum_limit:
+            return "The crowd is too much"
+
+    item = itertools.cycle(item_dict)
+    for key, value in item:
+        #key represents the item
+        #value represents the amount in stock
+        print(f"{key}: {value}")
+        if value == 0:
+            return "out of stock"
